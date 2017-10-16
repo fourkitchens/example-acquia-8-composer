92fa3f869a533922227135c72f4c20890d988534
Allan Chappell <generalredneck@gmail.com>
2017-10-16 08:15:35 -0500

    Adding some build commands.

---
+++ drush/drushrc.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @file
+ * Drush aliases.
+ *
+ * @see https://github.com/drush-ops/drush/blob/master/examples/example.drushrc.php
+ */
+
+$options['shell-aliases'] = [];
+
+$options['shell-aliases']['fresh'] = '!( ' . implode(" ) && \n ( ", [
+  "echo '\nInstalling PHP packages...'",
+  "cd ..; composer install; cd docroot",
+
+  "echo '\nRemoving old files and Extracting reference files, if any...'",
+  "bash -c '[ -f ../reference/files.tar.gz ] && \n rm -rf sites/default/files/* && \n tar -xzf ../reference/files.tar.gz -C sites/default/files || exit 0'",
+
+  "echo '\nDropping the database...'",
+  "drush sql-drop -y",
+
+  "echo '\nImporting reference database...'",
+  "gunzip < ../reference/nyu-wagner.sql.gz | sed '/INSERT INTO `cache.*` VALUES/d' | drush sql-cli",
+
+  "echo '\nSetting admin password...'",
+  "drush user-password 1 --password=admin",
+]) . " )";
+
+$options['shell-aliases']['local'] = "!( " . implode(" ) && \n ( ", [
+  "echo '\nUpdating database...'",
+  "drush updatedb -y",
+
+  "echo '\nRebuilding cache...'",
+  "drush cache-rebuild",
+
+  "echo '\nImporting configuration...'",
+  "drush config-import sync -y",
+
+  "echo '\nApplying pending entity schema updates...'",
+  "drush entity-updates -y",
+]) . " )";
+
+$options['shell-aliases']['prod'] = "!( " . implode(" ) && \n ( ", [
+  "echo '\nUpdating database...'",
+  "drush updatedb -y",
+
+  "echo '\nRebuilding cache...'",
+  "drush cache-rebuild",
+
+  "echo '\nImporting configuration...'",
+  "drush config-import sync -y",
+
+  "echo '\nApplying pending entity schema updates...'",
+  "drush entity-updates -y",
+]) . " )";
