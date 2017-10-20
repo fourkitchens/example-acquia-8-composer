02e7cbbaa0c82861a3dae9268fe3c2be047d8d6c
Allan Chappell <generalredneck@gmail.com>
2017-10-20 08:31:35 -0500

    Adding circle local settings.

---
+++ circle/settings.local.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file
+ * Secret configuration settings for the site.
+ */
+
+// Database.
+$databases = array(
+  'default' => array(
+    'default' => array(
+      'database' => 'circle_test',
+      'username' => 'ubuntu',
+      'password' => '',
+      'host' => '127.0.0.1',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
+
