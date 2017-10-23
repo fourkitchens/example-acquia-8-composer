cda0674b09f5dd458d34594194efbdf10e8e0290
Allan Chappell <generalredneck@gmail.com>
2017-10-23 13:42:07 -0500

    Updating the wait_for_cloud script to grab json a different way.

--- scripts/acquia/wait_for_cloud_done.sh
+++ scripts/acquia/wait_for_cloud_done.sh
@@ -38,31 +38,34 @@ if [[ $ac_command = "ac-code-deploy" ]] ||
 then
   echo "${0//.sh/}:"
   echo "    Command: $drush_command"
-
-  json="$($drush_command --format=json)"
-  id="$( php -r "\$json = json_decode('$json'); echo \$json->id;" )"
+  jsonfile=$(mktemp /tmp/wait_for_cloud_done.XXXXXX)
+  $drush_command --format=json > $jsonfile
+  id="$( php -r "\$json = json_decode(file_get_contents($jsonfile)); echo \$json->id;" )"
 
   echo "    Task: $id"
+  rm $jsonfile;
 
   oldlogs=""
   COUNTER=0
   # Hard timeout at 10 minutes
-  while [[ $state != "done" && $state != "error"  && $COUNTER -lt 200 ]]
+  while [[ $state != "done" && $state != "error"  && $COUNTER -lt 60 ]]
   do
     # Checking consumes resources, so wait for 3 seconds between checks.
     sleep 10
     let COUNTER=COUNTER+1
-    json="$(drush $1 ac-task-info $id --format=json)"
+    jsonfile=$(mktemp /tmp/wait_for_cloud_done.XXXXXX)
+    drush $1 ac-task-info $id --format=json > $jsonfile
     if [[ $? -gt 0 ]]
     then
       echo "drush ac-task-info Command Failed"
       exit 1
-    elif [[ -z "$json" ]]
+    elif [[ -z "$(cat $jsonfile)" ]]
     then
       echo "drush ac-task-info didn't return any data."
     fi
-    state="$( php -r "\$json = json_decode('$json'); echo \$json->state;" )"
-    newlogs="$( php -r "\$json = json_decode('$json'); echo \$json->logs;" )"
+    state="$( php -r "\$json = json_decode(file_get_contents($jsonfile)); echo \$json->state;" )"
+    newlogs="$( php -r "\$json = json_decode(file_get_contents($jsonfile)); echo \$json->logs;" )"
+    rm $jsonfile
     if [[ $newlogs != $oldlogs ]]
     then
       logdiff=${newlogs//"$oldlogs"/}
